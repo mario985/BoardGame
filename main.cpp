cbf9674612eaa48f598c37ca6a0bb3e6c23effe6
Youssef Hossam <155332489+YoussefHosssam@users.noreply.github.com>
2024-12-04 23:10:55 +0200

    Update main.cpp V3 add 4x4 XO

--- main.cpp
+++ main.cpp
@@ -14,9 +14,10 @@ int main() {
     cout << "Choose a game to play:\n";
     cout << "1. Four-In-Row\n";
     cout << "2. Numerical Tic-Tac-Toe\n";
-    cout << "3. 5x5 XO\n";
+    cout << "3. 5x5 Tic Tac Toe\n";
     cout << "4. Misere Tic-Tac-Toe\n";
-    cout <<"5.Ultimate_Tic_Tac_Toe\n";
+    cout << "5. Ultimate_Tic_Tac_Toe\n";
+    cout << "6. 4x4 Tic Tac Toe\n";
     cin >> gameChoice;
      if (gameChoice == 1) {
         Player<char>* charPlayers[2];
@@ -323,6 +324,75 @@ int main() {
         }
 
     }
+        else if (gameChoice == 6){
+            int choice;
+    Player<char>* players[2];
+    _4x4_X_O_Board<char>* B = new _4x4_X_O_Board<char>();
+    string playerXName, player2Name;
+
+    cout << "Welcome to 4x4 Tic Tac Toe Game. :)\n";
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            players[0] = new _4x4_X_O_Player<char>(playerXName, 'X');
+            break;
+        case 2:
+            players[0] = new _4x4_X_O_Random_Player<char>('X');
+            break;
+        case 3:
+            players[0] = new X_O_MinMax_Player<char>('X');
+            players[0]->setBoard(B);
+            break;
+        default:
+            cout << "Invalid choice for Player 1. Exiting the game.\n";
+            return 1;
+    }
+
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            players[1] = new _4x4_X_O_Player<char>(player2Name, 'O');
+            break;
+        case 2:
+            players[1] = new _4x4_X_O_Random_Player<char>('O');
+            break;
+        case 3:
+            players[1] = new X_O_MinMax_Player<char>('O');
+            players[1]->setBoard(B);
+            break;
+        default:
+            cout << "Invalid choice for Player 2. Exiting the game.\n";
+            return 1;
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> x_o_game(B, players);
+    x_o_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+
+        }
     else {
         cout << "Invalid game choice. Exiting.\n";
         return 1;
